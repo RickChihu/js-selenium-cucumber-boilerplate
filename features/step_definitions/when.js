a2b5081ff627aea85b67c89f52a9d927cbce1b10
Rick Chihu <ricardo.chihu@ihg.com>
2019-10-18 10:02:30 -0500

    Init with test working.

---
+++ features/step_definitions/when.js
@@ -0,0 +1,12 @@
+module.exports = function () {
+
+    this.When(/^I search Google for "([^"]*)"$/, function (searchQuery) {
+     
+        return helpers.loadPage('http://www.google.com').then(function() {
+     
+            // use a method on the page object which also returns a promise
+            return page.googleSearch.performSearch(searchQuery);
+        })
+    });
+
+};
